414f29e0247c09d1cf3330c74151fc857e12798d
owl4484 <rodeo23267286@gmail.com>
2025-08-18 11:28:03 +0900

    [Feat] Implement PieceManager (hand/placement flow)
    - 조각 핸드/배치 플로우 구현

---
+++ Assets/00.WorkSpace/KMS/Script/Manager/Class/PieceManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ================================
+// Project : DynamicBlock
+// Script  : PieceManager.cs
+// Desc    : 손패/조각 생성/소진
+// ================================
+public class PieceManager : MonoBehaviour, IManager
+{
+    [SerializeField] private PieceSetSO _pieceSet;
+    [SerializeField] private int _handSize = 3;
+
+    private readonly List<Vector2Int[]> _hand = new();
+
+    public void PreInit() { }
+    public void Init() { RefillHand(); }
+    public void PostInit() { }
+
+    public IReadOnlyList<Vector2Int[]> CurrentHand => _hand;
+
+    public void Consume(int index)
+    {
+        _hand.RemoveAt(index);
+        if (_hand.Count == 0) RefillHand();
+        EventBus.OnHandRefilled?.Invoke();
+    }
+
+    private void RefillHand()
+    {
+        _hand.Clear();
+        // 간단 랜덤(권장: Bag 시스템으로 편향 방지)
+        for (int i = 0; i < _handSize; i++)
+        {
+            var shape = _pieceSet.shapes[Random.Range(0, _pieceSet.shapes.Count)];
+            _hand.Add(shape);
+        }
+    }
+}
