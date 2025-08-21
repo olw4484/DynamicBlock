d7020e1c0345bc7e07eef1547c9e0443a2c515d8
eunjegolem <ppoppos@hotmail.com>
2025-08-21 17:44:23 +0900

    [Feat]JSON 저장 골조 추가
    JSON 저장 골조 추가
    클래식모드 최고 점수
    어드벤처모드 클리어 스테이지

---
+++ Assets/00.WorkSpace/LJH/Scripts/GameData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    // 클래식 모드
+    public int highScore;        // 최고 점수
+    public int lastScore;        // 마지막 플레이 점수
+    public int playCount;        // 플레이 횟수
+
+    // 어드벤처 모드
+    public int[] stageCleared;   // 0 = 미클리어, 1 = 클리어
+    public int[] stageScores;    // 각 스테이지 최고 점수
+}
