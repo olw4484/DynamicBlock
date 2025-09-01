327febd6af6282da1850ae5abc87118fab824ae1
Clean18 <cleanage95@gmail.com>
2025-09-01 17:33:32 +0900

    [Chore] 프로젝트명 롤백
    NinePuzzle > DefaultCompany

---
+++ Assets/00.WorkSpace/SJH/Scripts/AnalyticsManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Firebase;
+using Firebase.Extensions;
+using Firebase.Analytics;
+
+public class AnalyticsManager : MonoBehaviour
+{
+    public static AnalyticsManager Instance { get; private set; }
+	public FirebaseApp FirebaseApp { get; private set; }
+
+	void Awake()
+	{
+		Instance = this;
+
+		Init();
+
+		FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventLogin);
+	}
+
+	public void Init()
+	{
+		FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+			var dependencyStatus = task.Result;
+			if (dependencyStatus == Firebase.DependencyStatus.Available)
+			{
+				// Create and hold a reference to your FirebaseApp,
+				// where app is a Firebase.FirebaseApp property of your application class.
+				FirebaseApp = FirebaseApp.DefaultInstance;
+				Debug.Log("성공");
+				// Set a flag here to indicate whether Firebase is ready to use by your app.
+				FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventLogin);
+			}
+			else
+			{
+				Debug.LogError(System.String.Format(
+				  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+				// Firebase Unity SDK is not safe to use here.
+			}
+		});
+	}
+}
