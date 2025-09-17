68d4f5cedb33c80c2390fec580ebc32275b9ccde
Clean18 <cleanage95@gmail.com>
2025-09-17 09:51:49 +0900

    [Feat] 파이어스토어 sdk 추가

---
+++ Assets/00.WorkSpace/SJH/Scripts/FirestoreManager.cs
@@ -0,0 +1,69 @@
+﻿using Firebase.Extensions;
+using Firebase.Firestore;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirestoreManager : MonoBehaviour
+{
+	public static FirebaseFirestore Instance;
+
+	void Awake()
+	{
+		Instance = FirebaseFirestore.DefaultInstance;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Q)) WriteData();
+		if (Input.GetKeyDown(KeyCode.W)) ReadData();
+	}
+
+	void WriteData()
+	{
+		Debug.Log("데이터 추가 시도");
+
+		/* DB 계층구조
+		StageData
+		ㄴ Stage1 -- 현재 스테이지 키값이 필요함
+			ㄴSuccess : n
+			ㄴFailed : n
+		ㄴ Stage2
+		ㄴ  ...
+		ㄴ  ...
+		 */
+
+		CollectionReference usersRef = Instance.Collection("StageData");
+		DocumentReference docRef = usersRef.Document("Stage1");
+		Dictionary<string, object> user = new Dictionary<string, object>
+		{
+			{ "Success", UnityEngine.Random.Range(0, 100) },
+			{ "Failed", UnityEngine.Random.Range(0, 100) },
+		};
+		docRef.SetAsync(user).ContinueWithOnMainThread(task =>
+		{
+			Debug.Log("데이터 추가 성공");
+		});
+	}
+
+	void ReadData()
+	{
+		Debug.Log("데이터 읽기 시도");
+
+		CollectionReference usersRef = Instance.Collection("StageData");
+		usersRef.GetSnapshotAsync().ContinueWithOnMainThread(task =>
+		{
+			QuerySnapshot snapshot = task.Result;
+			foreach (DocumentSnapshot document in snapshot.Documents)
+			{
+				Dictionary<string, object> documentDictionary = document.ToDictionary();
+				Debug.Log(string.Format("{0} 첫 시도 성공확률 : {1:P0}",
+					document.Id ,
+					((double)(long)documentDictionary["Success"] / ((double)(long)documentDictionary["Success"] + (long)documentDictionary["Failed"]))
+					));
+			}
+
+			Debug.Log("데이터 읽기 성공");
+		});
+	}
+}
