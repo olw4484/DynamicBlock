173f21e1d266b82b34a2319a547ce0ed6a1ee8ce
Clean18 <cleanage95@gmail.com>
2025-09-19 14:18:38 +0900

    [Feat] 파이어스토어 db 업데이트 기능 추가

--- Assets/00.WorkSpace/SJH/Scripts/FirestoreManager.cs
+++ Assets/00.WorkSpace/SJH/Scripts/FirestoreManager.cs
@@ -2,15 +2,61 @@
 using Firebase.Firestore;
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class StageClearRate
+{
+	public int Rate;		// 스테이지 첫 시도시 성공 확률
+	public long Success;	// 성공
+	public long Failed;		// 실패
+}
 
 public class FirestoreManager : MonoBehaviour
 {
 	public static FirebaseFirestore Instance;
 
+	// test
+	[SerializeField] private TMP_Text _firestoreText;
+	[SerializeField] private FirebaseFirestore _firestore;
+	[SerializeField] private Button _initBtn;
+	[SerializeField] private TMP_Text _readText;
+	[SerializeField] private Button _readBtn;
+	[SerializeField] private TMP_Text _writeText;
+	[SerializeField] private Button _writeBtn;
+
+	[SerializeField] private bool _isReset = false;
+	[Header("스테이지 이름")]
+	[SerializeField] private string _stageName;
+	[Header("스테이지 클리어 여부")]
+	[SerializeField] private bool _isClear = false;
+
+	public void InitBtn()
+	{
+		_firestore = FirebaseFirestore.DefaultInstance;
+		Instance = FirebaseFirestore.DefaultInstance;
+		_firestoreText.text = _firestore?.ToString();
+
+		if (_isReset) _firestore.ClearPersistenceAsync(); // 캐시  초기화
+	}
+
+	public void ReadBtn()
+	{
+		ReadData();
+	}
+
+	public void WriteBtn()
+	{
+		WriteData();
+	}
+
+	// test
+
 	void Awake()
 	{
-		Instance = FirebaseFirestore.DefaultInstance;
+		//Instance = FirebaseFirestore.DefaultInstance;
 	}
 
 	void Update()
@@ -33,36 +79,56 @@ public class FirestoreManager : MonoBehaviour
 		ㄴ  ...
 		 */
 
-		CollectionReference usersRef = Instance.Collection("StageData");
-		DocumentReference docRef = usersRef.Document("Stage1");
-		Dictionary<string, object> user = new Dictionary<string, object>
+		if (Instance == null) return;
+		if (string.IsNullOrEmpty(_stageName)) return;
+		// 포인터
+		DocumentReference stageRef = Instance.Collection("StageData").Document(_stageName);
+
+		string clearKey = _isClear ? "Success" : "Failed";
+
+		var data = new Dictionary<string, object>()
 		{
-			{ "Success", UnityEngine.Random.Range(0, 100) },
-			{ "Failed", UnityEngine.Random.Range(0, 100) },
+			{ clearKey, FieldValue.Increment(1) }
 		};
-		docRef.SetAsync(user).ContinueWithOnMainThread(task =>
+		stageRef.UpdateAsync(data).ContinueWithOnMainThread(task =>
 		{
-			Debug.Log("데이터 추가 성공");
+			if (task.IsCompletedSuccessfully)
+			{
+				Debug.Log("업데이트 성공");
+			}
+			else
+			{
+				Debug.Log("업데이트 실패");
+			}
 		});
 	}
 
 	void ReadData()
 	{
 		Debug.Log("데이터 읽기 시도");
+		if (Instance == null) return;
+		// 포인터
+		if (string.IsNullOrEmpty(_stageName)) return;
+		DocumentReference docRef = Instance.Collection("StageData").Document(_stageName); // Document(string path)
 
-		CollectionReference usersRef = Instance.Collection("StageData");
-		usersRef.GetSnapshotAsync().ContinueWithOnMainThread(task =>
+		// Default : 서버 데이터 읽기, 실패시 캐시된 데이터 반환
+		// Cache : 캐시된 데이터 반환
+		// Server : 서버 데이터 읽기, 실패시 에러 반환
+		docRef.GetSnapshotAsync(Source.Cache).ContinueWithOnMainThread(task =>
 		{
-			QuerySnapshot snapshot = task.Result;
-			foreach (DocumentSnapshot document in snapshot.Documents)
+			DocumentSnapshot snapshot = task.Result;
+			
+			if (!snapshot.Exists)
 			{
-				Dictionary<string, object> documentDictionary = document.ToDictionary();
-				Debug.Log(string.Format("{0} 첫 시도 성공확률 : {1:P0}",
-					document.Id ,
-					((double)(long)documentDictionary["Success"] / ((double)(long)documentDictionary["Success"] + (long)documentDictionary["Failed"]))
-					));
+				_readText.text = $"{snapshot.Id} 데이터 읽기 실패";
+				Debug.Log($"{snapshot.Id} 데이터 읽기 실패");
+				return;
 			}
-
+			Dictionary<string, object> documentDictionary = snapshot.ToDictionary();
+			_readText.text = string.Format("{0} 첫 시도 성공확률 : {1:P0}",
+					snapshot.Id,
+					((double)(long)documentDictionary["Success"] / ((double)(long)documentDictionary["Success"] + (long)documentDictionary["Failed"]))
+					);
 			Debug.Log("데이터 읽기 성공");
 		});
 	}
